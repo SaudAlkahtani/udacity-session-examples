f07cd5871562f144560c3a501f3c772193d94749
Saud amer Alkahtani <saud.a.kahtani@gmail.com>
2018-10-24 00:56:35 +0300

    edited udacian name

--- session2/Udacian.py
+++ session2/Udacian.py
@@ -8,5 +8,5 @@ class Udacian:
     def print_udacain(self):
         print(self.name + "is Living in " + self.city + " and is studying " + self.nanodegree + " and enrolled in " + self.enrollment + ", he/she is " + self.status )
 enrollment = ("Mon virtual")
-u1 = Udacian("Mohammed","Riyadh",enrollment,"FSND","On track")
+u1 = Udacian("Saud","Riyadh",enrollment,"FSND","On track")
 u1.print_udacain()
