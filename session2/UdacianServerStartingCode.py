ca3262a78cff4be5620f4425556c41115696b1a5
Saud amer Alkahtani <saud.a.kahtani@gmail.com>
2018-10-28 23:56:19 +0300

    implemented post and get in the server

--- session2/UdacianServerStartingCode.py
+++ session2/UdacianServerStartingCode.py
@@ -10,31 +10,47 @@ memory = []
 form = '''<!DOCTYPE html>
   <title>Udacian</title>
   <form method="POST" action="http://localhost:8000/">
-    <textarea name="name">name</textarea>
+    <textarea name="name" placeholder="name"></textarea>
     <br>
-    <textarea name="city">city</textarea>
+    <textarea name="city" placeholder="city"></textarea>
     <br>
-    <textarea name="enrollment">enrollment</textarea>
+    <textarea name="enrollment" placeholder="enrollment"></textarea>
     <br>
-    <textarea name="nanodegree">nanodegree</textarea>
+    <textarea name="nanodegree" placeholder="nanodegree"></textarea>
     <br>
-    <textarea name="status">status</textarea>
+    <textarea name="status" placeholder="status"></textarea>
     <br>
     <button type="submit">Post it!</button>
   </form>
   <pre>
-{}
+  {}
   </pre>
 '''
 
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-    	#Submit the form
-      print("hi")
+      output=''
+      length = int(self.headers.get('Content-length', 0))  
+      data = self.rfile.read(length).decode()
+      name = parse_qs(data)["name"][0]
+      city = parse_qs(data)["city"][0]
+      enrollment = parse_qs(data)["enrollment"][0]
+      nanodegree = parse_qs(data)["nanodegree"][0]
+      status= parse_qs(data)["status"][0]
+      output = (name + " is Living in " + city + " and is studying " + nanodegree + " and enrolled in " + enrollment + ", he/she is " + status )
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html; charset=utf-8')
+      self.end_headers()
+      self.wfile.write(form.format(output).encode())
+      
     def do_GET(self):
     	#get the info and display it
-      print("hello")
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html; charset=utf-8')
+      self.end_headers()
+      self.wfile.write(form.format('').encode())
+    
 
 if __name__ == '__main__':
     server_address = ('', 8000)
